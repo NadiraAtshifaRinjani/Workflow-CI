cddf9b59d23c0c7b8770352f6a225cc8f3ffc7a5
NadiraAtshifaRinjani <nadiraasf19@gmail.com>
2025-12-19 17:10:03 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,7 +9,7 @@ import os
 
 #Load Data
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "dataset_preprocessing", "data_preprocessed.csv")
+DATA_PATH = os.path.join(BASE_DIR, "datasetrumah_preprocessing", "data_preprocessed.csv")
 
 df = pd.read_csv(DATA_PATH)
 
